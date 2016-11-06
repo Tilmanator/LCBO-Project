9546e3157b678c6870f018cc1c5e1d56da4c1f14
Tilman Lindig <soccer_14_rhs@live.ca>
2016-11-06 13:59:20 -0500

    Changed map style and removed deprecated stores

--- LCBO-Project/mapLoad.js
+++ LCBO-Project/mapLoad.js
@@ -1,4 +1,4 @@
-var total=0;
+var numStores=0;
 var stores=[];
 var currStore = null;
 
@@ -11,7 +11,7 @@ this.long=long;
 
 function loadStores(page) {
   var script = document.createElement('script');
-  script.src = 'http://lcboapi.com/stores?per_page=100;page='+page+';callback=loadMore';
+  script.src = 'http://lcboapi.com/stores?per_page=100;where_not=is_dead;page='+page+';callback=loadMore';
 
   script.async = true;
   document.head.appendChild(script);
@@ -21,10 +21,11 @@ function loadMore(response){
   for(var i=0; i< response.pager.current_page_record_count; i++){
     var r = response.result[i];
     var c = new storeInfo(r.id, r.name, r.latitude, r.longitude);
-    //alert(c);
     stores.push(c);
   }
-  total+= response.pager.current_page_record_count;
+
+  numStores+= response.pager.current_page_record_count;
+  
   if(!response.pager.is_final_page){
     loadStores(response.pager.current_page+1);
   }
